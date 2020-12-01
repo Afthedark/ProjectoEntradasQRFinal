d34d94bb1ff992ace5639ae203f91bea2b56464a
tito <tito@hotmail.com>
2020-11-30 23:32:43 -0400

    Reparando la cadena de conexion Mysql

--- dbcon.php
+++ dbcon.php
@@ -7,12 +7,5 @@ date_default_timezone_set('Asia/Manila');
 //$conn = new PDO('mysql:host=remotemysql.com;dbname=quKi93zdEh','quKi93zdEh','33Or8GjL2G');
 $conn = new PDO('mysql:host=sql10.freemysqlhosting.net;dbname=sql10378803','sql10378803','NJ1sx8wIYy');
 
-/*
-$server = "remotemysql.com";
-$user = "quKi93zdEh";
-$pass = "V9w6Q4mzA8";
-$dbname = "quKi93zdEh";  
 
-$conn = new Mysqli($server,$user,$pass,$dbname); 
-*/
 ?>
