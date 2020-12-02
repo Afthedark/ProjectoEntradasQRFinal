0b4b7295a9237d7b671d7ef0252334f762546611
tito <tito@hotmail.com>
2020-12-02 18:52:10 -0400

    Se fixeo el footer.php

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
                 </div>
                 <div class="col-sm-6 text-right">
                   
-                  <!-- Please do not remove the backlink to us unless you support further theme's development at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
+                  <!-- Please do not remove the backlink to us unless you support further theme's development at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :D-->
                 </div>
               </div>
             </div>
