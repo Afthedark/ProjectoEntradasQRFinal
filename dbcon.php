eb9c20426083af5f01a062a75cf508d5435eee95
tito <tito@hotmail.com>
2020-12-02 18:56:57 -0400

    Se modifo la cadena de conexion a base de datos ahora se usa mysql en un hosting

--- dbcon.php
+++ dbcon.php
@@ -4,8 +4,8 @@ date_default_timezone_set('Asia/Manila');
 
 //$conn = new PDO('mysql:host=localhost:33065;dbname=qrcodegen', 'root','');
 
-//$conn = new PDO('mysql:host=remotemysql.com;dbname=quKi93zdEh','quKi93zdEh','33Or8GjL2G');
-$conn = new PDO('mysql:host=sql10.freemysqlhosting.net;dbname=sql10378803','sql10378803','NJ1sx8wIYy');
+$conn = new PDO('mysql:host=remotemysql.com;dbname=quKi93zdEh','quKi93zdEh','33Or8GjL2G');
+//$conn = new PDO('mysql:host=sql10.freemysqlhosting.net;dbname=sql10378803','sql10378803','NJ1sx8wIYy');
 
 
 ?>
